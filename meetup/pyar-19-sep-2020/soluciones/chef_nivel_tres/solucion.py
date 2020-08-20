601958a49215bba9cd382660c281c972c381c6cc
sasha <augusto.kiel@gmail.com>
2020-08-19 22:02:39 -0300

    Arreglar error de solucion 3

--- meetup/pyar-19-sep-2020/soluciones/chef_nivel_tres/solucion.py
+++ meetup/pyar-19-sep-2020/soluciones/chef_nivel_tres/solucion.py
@@ -1,35 +1,82 @@
 import csv
-from collections import Counter, namedtuple
-
-Orden = namedtuple('Orden','orden cantidad estilo')
 
 class CocinarHuevos:
+    
+    def __init__(self, archivo='ordenes.txt'):
+        self.archivo =  archivo
+        self.lector = csv.reader
+        self.ordenes = []
 
-    PRECIOS = { 'duro' : 0.1, 
-                'frito': 0.2,
-              }
+    def ejecutar(self):
+        self.cargar_ordenes()
+        self.informar_preparacion_de_ordenes()
+        self.informar_total()
+
+    def informar_total(self):
+        print(f'El total a pagar es {self.total()} pesos.')
     
-    REPR = { 'duro'  : '\N{egg}',
-             'frito' : '\N{cooking}',
-           }
-    
-    @staticmethod
-    def ejecutar(archivo='ordenes.txt'):
-        with open(archivo) as f:
-            csv_reader = csv.reader(f)
-            ordenes = list(csv_reader)[1:]
-            
-            cuentas = Counter( row[2] for row in ordenes for _ in range(int(row[1].strip())) )
-            total = sum( CocinarHuevos.PRECIOS.get(k.strip(),0)*v for k,v in cuentas.items()  )
-            
-            CocinarHuevos.informar(ordenes)
-            print(f'El total a pagar es {total} pesos.')
+    def total(self):
+        return sum( orden.costo() for orden in self.ordenes  )
+
+    def cargar_ordenes(self):
+        with open(self.archivo) as f:
+            csv_reader = self.lector(f)
+            self.saltear_encabezado(csv_reader)
+            self.ordenes = [ Orden(*linea) for linea in csv_reader ]
             
     @staticmethod
-    def informar(ordenes):
-        for orden in ordenes:
-            orden = Orden(*orden)
-            x = ( f'huevo {CocinarHuevos.REPR.get(orden.estilo.strip())}'
-                   if int(orden.cantidad.strip()) == 1 
-                   else f'huevos {CocinarHuevos.REPR.get(orden.estilo.strip())}' )
-            print(f'La orden {orden.orden} es de {orden.cantidad.strip()} {x}')
+    def saltear_encabezado(csv_reader):
+        next(csv_reader)
+
+    def informar_preparacion_de_ordenes(self):
+        for orden in self.ordenes:
+            print(orden)
+
+
+class Orden:
+
+    def __init__(self, orden, cantidad, estilo):
+        self.orden = orden
+        self.cantidad = int(cantidad)
+        self.estilo = PreparacionHuevo.por(estilo.strip())
+
+    def __str__(self):
+        return f'La orden {self.orden} es de {self.cantidad} {self.rep_cantidad()} {self.estilo}'
+
+    def costo(self):
+        return self.cantidad * self.estilo.costo()
+
+    def rep_cantidad(self):
+        return 'huevo' if self.cantidad == 1 else 'huevos'
+
+class PreparacionHuevo:
+
+    _costo = 0.0
+    _emoji = 'Desconocida'
+
+    @classmethod
+    def por(cls, estilo):
+        if estilo == 'duro':
+            return HuevoDuro()
+        elif estilo == 'frito':
+            return HuevoFrito()
+        else:
+            return PreparacionDesconocida()
+
+    def costo(self):
+        return self._costo
+    
+    def __str__(self):
+        return self._emoji
+
+class HuevoDuro(PreparacionHuevo):
+    _costo = 0.1
+    _emoji = '\N{egg}'
+
+
+class HuevoFrito(PreparacionHuevo):
+    _costo = 0.2
+    _emoji = '\N{cooking}'
+    
+class PreparacionDesconocida(PreparacionHuevo):
+    pass
